5c1d1149a76ed138b74c22b2300ad7e5c9c83b81
ThomasRgl <thomas.roglin@free.fr>
2021-08-09 14:20:36 +0200

    collisions

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,168 @@
 
 static const float VIEW_SIZE = 1000.f;
 
+class Circuit : public sf::Drawable {
+private:
+    size_t sizeCircuit;
+public:
+    std::vector<sf::Vertex> vertices;
+    Circuit(std::string fileName){
+        std::vector<std::pair<std::string, std::vector<int>>> test = read_csv("circuits/"+fileName);
+        if(test[0].second.size() != test[1].second.size())
+        exit(-1);
+        if( test[0].second.size() < 6 || test[0].second.size() % 2 == 1)
+        exit(-1);
+
+
+        for (int i = 0; i != test[0].second.size(); i++){
+            std::cout << test[0].second[i] << ',' <<  test[1].second[i] << std::endl;
+            sf::Vertex vertex;
+            vertex.position = sf::Vector2f(test[0].second[i], test[1].second[i]);
+            vertex.color = sf::Color(128,128,128);
+            vertices.push_back(vertex);
+            // vertices.push_back(sf::Vertex(test[0].second[i], test[1].second[i]));
+        }
+
+        sizeCircuit = (vertices.size() - 2)/2;
+    }
+    Circuit(){}
+
+    std::vector<sf::Vertex>* getVertices(){
+        return &vertices;
+    }
+
+    sf::Vertex GetVertices(size_t i){
+        if(i > vertices.size() || i < 0)
+        exit(-1);
+        return vertices[i];
+    }
+
+    void highlightZone( size_t n ){
+        if( n < 0 || n >= sizeCircuit ) {
+            std::cout << "impossible de surligner cette zone" << std::endl;
+            return;
+        }
+
+        for (int i = 0; i != vertices.size(); i++){
+            vertices[i].color = sf::Color(128,128,128);
+        }
+
+        vertices[n * 2 + 0].color = sf::Color::Green;
+        vertices[n * 2 + 1].color = sf::Color::Green;
+        vertices[n * 2 + 2].color = sf::Color::Green;
+        vertices[n * 2 + 3].color = sf::Color::Green;
+    }
+
+    // void checkZone(Voiture voiture){
+    //     size_t zone = voiture.getZone();
+    //
+    //     sf::Vertex A = voiture.getPosition();
+    //     sf::Vertex B = voiture.getOldPosition();
+    //     sf::Vertex C = vertices[2 * zone]; // zone precedante
+    //     sf::Vertex D= vertices[2 * zone + 1];
+    //
+    //     if(intersect(A,B,C,D)){
+    //         if(zone == 0)
+    //             voiture.zone = (vertices.size() - 4)/2 - 1;
+    //         else
+    //             zone -= 1;
+    //     }
+    // }
+    size_t getSize(){
+        return sizeCircuit;
+    }
+    // void export(){}
+    // virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const{
+        target.draw(&vertices[0], vertices.size(), sf::TriangleStrip, states);
+    }
+};
+
+
+boost::optional<sf::Vertex> intersect( sf::Vertex A, sf::Vertex B, sf::Vertex C, sf::Vertex D){
+
+    /*
+    M
+    [ B.x - A.x , C.x - D.x ]
+    [ B.y - A.y , C.y - D.y ]
+
+    Y
+    (C.x - A.x, C.y - A.y)
+
+    */
+
+    float M[2][2] = {{B.position.x - A.position.x , C.position.x - D.position.x},{B.position.y - A.position.y , C.position.y - D.position.y}};
+    float V[2] = {C.position.x - A.position.x, C.position.y - A.position.y};
+    float det = M[0][0] * M[1][1] - M[1][0] * M[0][1];
+    if( det == 0){
+        // std::cout << "les 2 segments sont //" << std::endl;
+        return boost::none;
+    }
+
+    float inv_det = 1 / det;
+    float M_inv[2][2] = {inv_det * M[1][1], inv_det * -M[0][1], inv_det * -M[1][0], inv_det * M[0][0]};
+
+    // M_inv * Y
+    float X[2] = { M_inv[0][0] * V[0] + M_inv[0][1] * V[1], M_inv[1][0] * V[0] + M_inv[1][1] * V[1]  };
+
+    //
+    if( !(0 < X[0] && X[0] < 1 && 0 < X[1] && X[1] < 1)){
+        // std::cout << "les 2 segments ne sont pas // mais ne se croisent pas" << std::endl;
+        return boost::none;
+    }
+
+    sf::Vertex S ;
+    S.position.x = A.position.x + X[0] * (B.position.x - A.position.x);
+    S.position.y = A.position.y + X[0] * (B.position.y - A.position.y);
+
+    return S;
+}
+
+boost::optional<sf::Vertex> intersectV2( sf::Vertex A, sf::Vertex B, sf::Vertex C, sf::Vertex D){
+
+    /*
+    M
+    [ B.x - A.x , C.x - D.x ]
+    [ B.y - A.y , C.y - D.y ]
+
+    Y
+    (C.x - A.x, C.y - A.y)
+
+    */
+
+    float M[2][2] = {{B.position.x - A.position.x , C.position.x - D.position.x},{B.position.y - A.position.y , C.position.y - D.position.y}};
+    float V[2] = {C.position.x - A.position.x, C.position.y - A.position.y};
+    float det = M[0][0] * M[1][1] - M[1][0] * M[0][1];
+    if( det == 0){
+        // std::cout << "les 2 segments sont //" << std::endl;
+        return boost::none;
+    }
+
+    float inv_det = 1 / det;
+    float M_inv[2][2] = {inv_det * M[1][1], inv_det * -M[0][1], inv_det * -M[1][0], inv_det * M[0][0]};
+
+    // M_inv * Y
+    float X[2] = { M_inv[0][0] * V[0] + M_inv[0][1] * V[1], M_inv[1][0] * V[0] + M_inv[1][1] * V[1]  };
+
+    //
+    if( !(0 <= X[0] && X[0] <= 1 && 0 <= X[1] && X[1] <= 1)){
+        // std::cout << "les 2 segments ne sont pas // mais ne se croisent pas" << std::endl;
+        return boost::none;
+    }
+
+    sf::Vertex S ;
+    S.position.x = A.position.x + X[0] * (B.position.x - A.position.x);
+    S.position.y = A.position.y + X[0] * (B.position.y - A.position.y);
+
+    return S;
+}
+
 
 class Voiture  {
 
 public:
     sf::Vector2f position;
-    sf::Vector2f OldPosition;
+    sf::Vector2f oldPosition;
 
     sf::Texture texture;
     sf::Sprite sprite;
@@ -31,7 +187,7 @@ public:
     Voiture(sf::Texture _texture_, sf::Vector2f _position_ ){
         texture = _texture_;
         position = _position_;
-        OldPosition = position;
+        oldPosition = position;
 
         //
         speed = 0;
@@ -63,7 +219,7 @@ public:
 
     void update(double deltaTime){
 
-        OldPosition = position;
+        oldPosition = position;
 
         //frotement
         // acceleration += -speed * DeltaFrotement * 1/deltaTime * 0.001;
@@ -86,6 +242,80 @@ public:
         sprite.setPosition(position);
     }
 
+    void checkZone(Circuit map){
+
+        sf::Vertex A = position;
+        sf::Vertex B = oldPosition;
+        sf::Vertex C = map.GetVertices(2 * zone);
+        sf::Vertex D = map.GetVertices(2 * zone + 1);
+
+        if(intersectV2(A,B,C,D)){
+            do {
+                if(zone == 0)
+                    zone = map.getSize() - 1;
+                else
+                    zone -= 1;
+
+                C = map.GetVertices(2 * zone); // zone precedante
+                D = map.GetVertices(2 * zone + 1);
+
+            } while( intersectV2(A,B,C,D) );
+
+            return;
+        }
+
+
+        C = map.GetVertices(2 * zone + 2); // zone suivante
+        D = map.GetVertices(2 * zone + 3);
+
+        if(intersectV2(A,B,C,D)){
+            do {
+                if(zone == map.getSize() - 1)
+                    zone = 0;
+                else
+                    zone += 1;
+
+                C = map.GetVertices(2 * zone + 2); // zone suivante
+                D = map.GetVertices(2 * zone + 3);
+
+            } while( intersectV2(A,B,C,D) );
+
+            return;
+        }
+
+    }
+
+    void collide(Circuit map){
+
+        sf::Vertex A = position;
+        sf::Vertex B = oldPosition;
+        sf::Vertex C = map.GetVertices(2 * zone);
+        sf::Vertex D = map.GetVertices(2 * zone + 2);
+
+        boost::optional<sf::Vertex> I = intersectV2(A,B,C,D);
+        if( I ){
+            position = oldPosition;
+            speed = 0;
+            std::cout << "Collision - x :  " << I->position.x << "  y : " << I->position.y << std::endl;
+
+            }
+
+
+
+        C = map.GetVertices(2 * zone + 1);
+        D = map.GetVertices(2 * zone + 3);
+
+        I = intersectV2(A,B,C,D);
+        if( I ){
+            position = oldPosition;
+            speed = 0;
+            std::cout << "Collision - x :  " << I->position.x << "  y : " << I->position.y << std::endl;
+            }
+
+        return;
+    }
+
+
     sf::Sprite getSprite(){
         return sprite;
     }
@@ -94,128 +324,45 @@ public:
         return zone;
     }
 
-};
-
-class Circuit : public sf::Drawable {
-private:
-public:
-    std::vector<sf::Vertex> vertices;
-    Circuit(std::string fileName){
-        std::vector<std::pair<std::string, std::vector<int>>> test = read_csv("circuits/"+fileName);
-        if(test[0].second.size() != test[1].second.size())
-        exit(-1);
-        if( test[0].second.size() < 6 || test[0].second.size() % 2 == 1)
-        exit(-1);
-
-        for (int i = 0; i != test[0].second.size(); i++){
-            std::cout << test[0].second[i] << ',' <<  test[1].second[i] << std::endl;
-            sf::Vertex vertex;
-            vertex.position = sf::Vector2f(test[0].second[i], test[1].second[i]);
-            vertex.color = sf::Color(128,128,128);
-            vertices.push_back(vertex);
-            // vertices.push_back(sf::Vertex(test[0].second[i], test[1].second[i]));
-        }
-    }
-    Circuit(){}
-
-    std::vector<sf::Vertex>* getVertices(){
-        return &vertices;
+    sf::Vertex getPosition(){
+        return position;
     }
 
-    void highlightZone( size_t n ){
-        if( n < 0 || n > ((vertices.size() - 4)/2 - 1) ) {
-            std::cout << "impossible de surligner cette zone" << std::endl;
-            return;
-        }
-
-        for (int i = 0; i != vertices.size(); i++){
-            vertices[i].color = sf::Color(128,128,128);
-        }
-
-        vertices[n * 2 + 0].color = sf::Color::Green;
-        vertices[n * 2 + 1].color = sf::Color::Green;
-        vertices[n * 2 + 2].color = sf::Color::Green;
-        vertices[n * 2 + 3].color = sf::Color::Green;
-    }
-
-    void checkZone(Voiture voiture){
-        size_t zone = voiture.getZone();
-
-    }
-
-    // void export(){}
-    // virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const{
-        target.draw(&vertices[0], vertices.size(), sf::TriangleStrip, states);
+    sf::Vertex getOldPosition(){
+        return oldPosition;
     }
 };
 
-boost::optional<sf::Vertex> intersect( sf::Vertex A, sf::Vertex B, sf::Vertex C, sf::Vertex D){
-    /*
-    M
-    [ B.x - A.x , C.x - D.x ]
-    [ B.y - A.y , C.y - D.y ]
 
-    Y
-    (C.x - A.x, C.y - A.y)
-
-    */
-
-    float M[2][2] = {{B.position.x - A.position.x , C.position.x - D.position.x},{B.position.y - A.position.y , C.position.y - D.position.y}};
-    float V[2] = {C.position.x - A.position.x, C.position.y - A.position.y};
-    float det = M[0][0] * M[1][1] - M[1][0] * M[0][1];
-    if( det == 0){
-        std::cout << "les 2 segments sont //" << std::endl;
-        return boost::none;
-    }
-
-    float inv_det = 1 / det;
-    float M_inv[2][2] = {inv_det * M[1][1], inv_det * -M[0][1], inv_det * -M[1][0], inv_det * M[0][0]};
-
-    // M_inv * Y
-    float X[2] = { M_inv[0][0] * V[0] + M_inv[0][1] * V[1], M_inv[1][0] * V[0] + M_inv[1][1] * V[1]  };
-
-    //
-    if( !(0 < X[0] && X[0] < 1 && 0 < X[1] && X[1] < 1)){
-        std::cout << "les 2 segments ne sont pas // mais ne se croisent pas" << std::endl;
-        return boost::none;
-    }
-
-    sf::Vertex S ;
-    S.position.x = A.position.x + X[0] * (B.position.x - A.position.x);
-    S.position.y = A.position.y + X[0] * (B.position.y - A.position.y);
-
-    return S;
-}
 
 int main()
 {
 
-    sf::Vertex A;
-    sf::Vertex B;
-    sf::Vertex C;
-    sf::Vertex D;
-
-    A.position.x = 0;
-    A.position.y = 0.5;
-
-    B.position.x = 1;
-    B.position.y = 0.5;
-
-    C.position.x = 0;
-    C.position.y = 0;
-
-    D.position.x = 0;
-    D.position.y = 1;
-
-    boost::optional<sf::Vertex> S = intersect(A,B,C,D);
-
-    if(S){
-        std::cout << "x : " << S->position.x << std::endl;
-        std::cout << "y : " << S->position.y << std::endl;
-    }
-
-    return 0 ;
+    // sf::Vertex A;
+    // sf::Vertex B;
+    // sf::Vertex C;
+    // sf::Vertex D;
+    //
+    // A.position.x = 0;
+    // A.position.y = 0.5;
+    //
+    // B.position.x = -1;
+    // B.position.y = 0.5;
+    //
+    // C.position.x = 0;
+    // C.position.y = 0;
+    //
+    // D.position.x = 0;
+    // D.position.y = 1;
+    //
+    // boost::optional<sf::Vertex> S = intersect(A,B,C,D);
+    //
+    // if(S){
+    //     std::cout << "x : " << S->position.x << std::endl;
+    //     std::cout << "y : " << S->position.y << std::endl;
+    // }
+    //
+    // return 0 ;
 
 
 
@@ -267,10 +414,10 @@ int main()
     float pos_y = ( (*vertices)[0].position.y + (*vertices)[1].position.y + (*vertices)[2].position.y + (*vertices)[3].position.y)/4;
     Voiture voiture(voiture_texture, sf::Vector2f( pos_x, pos_y ));
 
-    (*vertices)[0].color = sf::Color::Green;
-    (*vertices)[1].color = sf::Color::Green;
-    (*vertices)[2].color = sf::Color::Green;
-    (*vertices)[3].color = sf::Color::Green;
+    // (*vertices)[0].color = sf::Color::Green;
+    // (*vertices)[1].color = sf::Color::Green;
+    // (*vertices)[2].color = sf::Color::Green;
+    // (*vertices)[3].color = sf::Color::Green;
 
     // map.vertices[0].color = sf::Color::Green;
     // map.vertices[1].color = sf::Color::Green;
@@ -380,6 +527,10 @@ int main()
 
         //Update
         voiture.update(deltaTime);
+        voiture.checkZone(map);
+        voiture.collide(map);
+        map.highlightZone(voiture.getZone());
+        // map.checkZone(voiture);
 
         //Collision
 
